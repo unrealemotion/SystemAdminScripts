f8321d2ff535cb921281e85c28026831ce6d426b
unrealemotion <59278759+unrealemotion@users.noreply.github.com>
2024-12-12 20:31:50 +0700

    Update Evtx2xml1.2.ps1
    Now can sel output dir

--- Evtx2xml1.1.ps1
+++ Evtx2xml1.1.ps1
@@ -1,5 +1,8 @@
-﻿# Prompt for the directory containing the .evtx files
+# Prompt for the directory containing the .evtx files
 $directoryPath = Read-Host -Prompt "Enter the directory containing the .evtx files"
+
+# Prompt for the output directory
+$outputDirectory = Read-Host -Prompt "Enter the output directory"
 
 # Prompt for the number to append to the file names
 $appendNumber = Read-Host -Prompt "Enter the number to append to the file names"
@@ -15,8 +18,11 @@ foreach ($evtxFile in $evtxFiles) {
     # Construct the new .clixml file name with the appended number
     $clixmlFileName = $baseName + "_" + $appendNumber + ".clixml"
 
+    # Construct the full path for the output .clixml file
+    $clixmlFilePath = Join-Path -Path $outputDirectory -ChildPath $clixmlFileName
+
     # Convert the .evtx file to .clixml
-    Get-WinEvent -Path $evtxFile.FullName | Export-Clixml -Path $clixmlFileName
+    Get-WinEvent -Path $evtxFile.FullName | Export-Clixml -Path $clixmlFilePath
 }
 
 Write-Host "Conversion complete!"
